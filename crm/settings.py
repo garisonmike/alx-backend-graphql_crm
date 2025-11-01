6c73d69f7712ff54f301310a87a49658063ae639
garisonmike <garisonmike006@gmail.com>
2025-11-01 12:26:52 +0300

    included redis and celery services

--- crm/settings.py
+++ crm/settings.py
@@ -18,8 +18,10 @@ CRONJOBS = [
 ]
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+import os
+
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
